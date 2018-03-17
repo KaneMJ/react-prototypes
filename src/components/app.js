21049f16d5d0304056f3de7221ab850c1b098742
KaneMJ <matthewjkane@live.com>
2018-03-16 22:51:24 -0700

    finished

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table'
 
+const students = [
+    {
+        student: "Bruce Wayne",
+        course: "Criminal Justice",
+        grade: 94
+    },
+    {
+        student: "Clark Kent",
+        course: "Being Lame",
+        grade: 100
+    },
+    {
+        student: "Peter Parker",
+        course: "Biology",
+        grade: 99
+    }
+];
+
+export default (props) => { 
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
 
